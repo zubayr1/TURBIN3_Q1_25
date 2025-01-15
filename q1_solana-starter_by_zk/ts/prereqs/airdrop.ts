503eafd45426716a22602894907dda4e2cb5aa76
zubayr1 <write2zubayr@gmail.com>
2025-01-15 23:28:45 +0100

    Fix

--- q1_solana-starter_by_zk/ts/prereqs/airdrop.ts
+++ q1_solana-starter_by_zk/ts/prereqs/airdrop.ts
@@ -1,5 +1,5 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
-import wallet from "./dev-wallet.json"
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import wallet from "../dev-wallet.json";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -8,11 +8,14 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
-    try {
-        const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+  try {
+    const txhash = await connection.requestAirdrop(
+      keypair.publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
     console.log(`Success! Check out your TX here: 
     https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
--- q1_solana-starter_by_zk/ts/prereqs/enroll.ts
+++ q1_solana-starter_by_zk/ts/prereqs/enroll.ts
@@ -1,7 +1,14 @@
-import { Connection, Keypair, SystemProgram, PublicKey } from "@solana/web3.js"
-import { Program, Wallet, AnchorProvider, Address } from "@project-serum/anchor"
+import { Connection, Keypair, SystemProgram, PublicKey } from "@solana/web3.js";
+// import {
+//   Program,
+//   Wallet,
+//   AnchorProvider,
+//   Address,
+// } from "@project-serum/anchor";
+import { Program, Wallet, AnchorProvider, Address } from "@coral-xyz/anchor";
+
 import { WbaPrereq, IDL } from "../programs/wba_prereq";
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-wallet.json";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -13,31 +20,39 @@ const connection = new Connection("https://api.devnet.solana.com");
 const github = Buffer.from("testaccount", "utf8");
 
 // Create our anchor provider
-const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed"});
+const provider = new AnchorProvider(connection, new Wallet(keypair), {
+  commitment: "confirmed",
+});
 
 // Create our program
-const program = new Program<WbaPrereq>(IDL, "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1" as Address, provider);
+const program = new Program<WbaPrereq>(
+  IDL,
+  "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1" as Address,
+  provider
+);
 
 // Create the PDA for our enrollment account
 const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
-const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(enrollment_seeds, program.programId);
+const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
+  enrollment_seeds,
+  program.programId
+);
 
 // Execute our enrollment transaction
 (async () => {
-    try {
-        const txhash = await program.methods
-        .complete(github)
-        .accounts({
-            signer: keypair.publicKey,
-            prereq: enrollment_key,
-            systemProgram: SystemProgram.programId,
-        })
-        .signers([
-            keypair
-        ]).rpc();
-        console.log(`Success! Check out your TX here: 
+  try {
+    const txhash = await program.methods
+      .complete(github)
+      .accounts({
+        signer: keypair.publicKey,
+        prereq: enrollment_key,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(`Success! Check out your TX here: 
         https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
