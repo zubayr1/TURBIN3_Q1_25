ec9470f9633b3eed23974720375237135356e16b
zubayr1 <write2zubayr@gmail.com>
2025-01-25 19:28:33 +0100

    update transfer ownership

--- capstone_project/one_cs/anchor/programs/one_cs/src/instructions/transfer_ownership.rs
+++ capstone_project/one_cs/anchor/programs/one_cs/src/instructions/transfer_ownership.rs
@@ -17,39 +17,65 @@ pub struct TransferOwnership<'info> {
     )]
     pub encapsulated_data: Account<'info, PermissionData>,
 
+    #[account(
+      init_if_needed,
+      payer = owner,
+      space = 8 + DelegatedOwner::INIT_SPACE,
+      seeds = [b"delegated_owner", crate::ID.as_ref(), label.as_ref()],
+      bump
+    )]
+    pub delegated_owner: Account<'info, DelegatedOwner>,
+
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> TransferOwnership<'info> {
-    pub fn transfer_ownership(&mut self, _label: String, new_owner: Pubkey) -> Result<()> {
+    pub fn transfer_ownership(
+        &mut self,
+        _label: String,
+        new_owner: Pubkey,
+        ownership_time: u64,
+        bumps: &TransferOwnershipBumps,
+    ) -> Result<()> {
         let encapsulated_data = &mut self.encapsulated_data;
 
-        let old_owner = encapsulated_data.owner;
-        encapsulated_data.owner = new_owner;
+        // Check if instant delegation or future delegation
+        let current_time = Clock::get().unwrap().unix_timestamp as u64;
+        if ownership_time < current_time {
+            let old_owner = encapsulated_data.owner;
+            encapsulated_data.owner = new_owner;
 
-        // Update the old owner's permission to admin
-        if let Some(old_owner_permission) = encapsulated_data
-            .permissions
-            .iter_mut()
-            .find(|p| p.wallet == old_owner)
-        {
-            old_owner_permission.role = Role::Admin;
-        }
+            // Update the old owner's permission to admin
+            if let Some(old_owner_permission) = encapsulated_data
+                .permissions
+                .iter_mut()
+                .find(|p| p.wallet == old_owner)
+            {
+                old_owner_permission.role = Role::Admin;
+            }
 
-        // Update the new owner's permission to owner
-        if let Some(new_owner_permission) = encapsulated_data
-            .permissions
-            .iter_mut()
-            .find(|p| p.wallet == new_owner)
-        {
-            new_owner_permission.role = Role::Owner;
+            // Update the new owner's permission to owner
+            if let Some(new_owner_permission) = encapsulated_data
+                .permissions
+                .iter_mut()
+                .find(|p| p.wallet == new_owner)
+            {
+                new_owner_permission.role = Role::Owner;
+            } else {
+                // Add the new owner's permission to the permission list
+                encapsulated_data.permissions.push(Permission {
+                    role: Role::Owner,
+                    wallet: new_owner,
+                    start_time: 0,
+                    end_time: 0,
+                });
+            }
         } else {
-            // Add the new owner's permission to the permission list
-            encapsulated_data.permissions.push(Permission {
-                role: Role::Owner,
-                wallet: new_owner,
-                start_time: 0,
-                end_time: 0,
+            // Create a new delegated owner account
+            self.delegated_owner.set_inner(DelegatedOwner {
+                new_owner,
+                ownership_time,
+                bump: bumps.delegated_owner,
             });
         }
         Ok(())
