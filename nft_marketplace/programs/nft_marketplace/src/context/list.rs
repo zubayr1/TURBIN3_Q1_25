a5e31b1ac6de7a3cfcb1f72422b98a25a65e97cd
zubayr1 <write2zubayr@gmail.com>
2025-01-29 19:35:01 +0100

    implement listing

---
+++ nft_marketplace/programs/nft_marketplace/src/context/list.rs
@@ -0,0 +1,103 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{
+    transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
+};
+
+use anchor_spl::metadata::{MasterEditionAccount, Metadata, MetadataAccount};
+
+use crate::state::{Listing, Marketplace};
+
+#[derive(Accounts)]
+pub struct List<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(
+        seeds = [b"marketplace", marketplace.key().as_ref()],
+        bump = marketplace.bump
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+
+    pub maker_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = maker_mint,
+        associated_token::authority = maker,
+    )]
+    pub maker_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = maker_mint,
+        associated_token::authority = listing,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = maker,
+        space = 8 + Listing::INIT_SPACE,
+        seeds = [marketplace.key().as_ref(), maker_mint.key().as_ref()],
+        bump
+    )]
+    pub listing: Account<'info, Listing>,
+
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [b"metadata", metadata_program.key().as_ref(), maker_mint.key().as_ref()],
+        seeds::program = metadata_program.key(),
+        bump,
+        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().verified == true,
+    )]
+    pub metadata: Account<'info, MetadataAccount>,
+
+    #[account(
+        seeds = [b"metadata", metadata_program.key().as_ref(), maker_mint.key().as_ref()],
+        seeds::program = metadata_program.key(),
+        bump,
+    )]
+    pub master_edition: Account<'info, MasterEditionAccount>,
+
+    pub metadata_program: Program<'info, Metadata>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> List<'info> {
+    pub fn initialize_listing(&mut self, price: u64, bumps: &ListBumps) -> Result<()> {
+        self.listing.set_inner(Listing {
+            maker: self.maker.key(),
+            mint: self.maker_mint.key(),
+            price,
+            bump: bumps.listing,
+        });
+        Ok(())
+    }
+
+    pub fn deposit_nft(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.maker_ata.to_account_info(),
+            to: self.vault.to_account_info(),
+            authority: self.maker.to_account_info(),
+            mint: self.maker_mint.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_ctx, 1, self.maker_mint.decimals)?;
+
+        Ok(())
+    }
+}
