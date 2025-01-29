7d43a934ed35e2c2e46561fda6a193f968c775a3
zubayr1 <write2zubayr@gmail.com>
2025-01-29 19:35:15 +0100

    implement delisting

---
+++ nft_marketplace/programs/nft_marketplace/src/context/delist.rs
@@ -0,0 +1,78 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{
+    transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
+};
+
+// use anchor_spl::metadata::{MasterEditionAccount, Metadata, MetadataAccount};
+
+use crate::state::{Listing, Marketplace};
+
+#[derive(Accounts)]
+pub struct Delist<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(
+        seeds = [b"marketplace", marketplace.key().as_ref()],
+        bump = marketplace.bump
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+
+    pub maker_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = maker_mint,
+        associated_token::authority = maker,
+    )]
+    pub maker_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = maker_mint,
+        associated_token::authority = listing,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        close = maker,
+        seeds = [marketplace.key().as_ref(), maker_mint.key().as_ref()],
+        bump = listing.bump,
+    )]
+    pub listing: Account<'info, Listing>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Delist<'info> {
+    pub fn withdraw_nft(&mut self) -> Result<()> {
+        let seeds = &[
+            &self.marketplace.key().to_bytes()[..],
+            &self.maker_mint.key().to_bytes()[..],
+            &[self.listing.bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.vault.to_account_info(),
+            to: self.maker_ata.to_account_info(),
+            authority: self.listing.to_account_info(),
+            mint: self.maker_mint.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer_checked(cpi_ctx, 1, self.maker_mint.decimals)?;
+
+        Ok(())
+    }
+}
