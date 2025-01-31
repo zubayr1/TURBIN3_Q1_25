bdb414441558d1e98796e2259a69a69af28d0c5b
zubayr1 <write2zubayr@gmail.com>
2025-01-31 20:03:42 +0100

    update stake and unstake

---
+++ nft-staking/programs/nft-staking/src/contexts/stake.rs
@@ -0,0 +1,129 @@
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::metadata::mpl_token_metadata::instructions::{
+    FreezeDelegatedAccountCpi, FreezeDelegatedAccountCpiAccounts,
+};
+use anchor_spl::metadata::{MasterEditionAccount, Metadata, MetadataAccount};
+use anchor_spl::token::{approve, Approve, Mint, Token, TokenAccount};
+
+use crate::errors::StakeError;
+use crate::states::{StakeAccount, StakeConfig, UserAccount};
+
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub mint: Account<'info, Mint>,
+    pub collection_mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = user
+    )]
+    pub mint_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        seeds = [b"metadata", metadata_program.key().as_ref(), mint.key().as_ref()],
+        seeds::program = metadata_program.key(),
+        bump,
+        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().verified == true,
+    )]
+    pub metadata: Account<'info, MetadataAccount>,
+
+    pub metadata_program: Program<'info, Metadata>,
+
+    #[account(
+        seeds = [b"metadata", metadata_program.key().as_ref(), mint.key().as_ref(), b"edition"],
+        seeds::program = metadata_program.key(),
+        bump,
+    )]
+    pub edition: Account<'info, MasterEditionAccount>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump
+    )]
+    pub config_account: Account<'info, StakeConfig>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + StakeAccount::INIT_SPACE,
+        seeds = [b"stake", mint.key().as_ref(), config_account.key().as_ref()],
+        bump
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"user", user.key().as_ref()],
+        bump = user_account.bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Stake<'info> {
+    pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
+        require!(
+            self.user_account.amount_stake < self.config_account.max_stake,
+            StakeError::MaxStakeReached
+        );
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Approve {
+            to: self.mint_ata.to_account_info(),
+            delegate: self.stake_account.to_account_info(),
+            authority: self.user.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        approve(cpi_ctx, 1)?;
+
+        let seeds = &[
+            b"stake",
+            self.mint.to_account_info().key.as_ref(),
+            self.config_account.to_account_info().key.as_ref(),
+            &[bumps.stake_account],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let delegate = &self.stake_account.to_account_info();
+        let token_account = &self.mint_ata.to_account_info();
+        let edition = &self.edition.to_account_info();
+        let mint = &self.mint.to_account_info();
+        let token_program = &self.token_program.to_account_info();
+        let metadata_program = &self.metadata_program.to_account_info();
+
+        FreezeDelegatedAccountCpi::new(
+            metadata_program,
+            FreezeDelegatedAccountCpiAccounts {
+                delegate,
+                token_account,
+                edition,
+                mint,
+                token_program,
+            },
+        )
+        .invoke_signed(signer_seeds);
+
+        self.stake_account.set_inner(StakeAccount {
+            owner: self.user.key(),
+            mint: self.mint.key(),
+            staked_at: Clock::get().unwrap().unix_timestamp,
+            bump: bumps.stake_account,
+        });
+
+        self.user_account.amount_stake += 1;
+
+        Ok(())
+    }
+}
