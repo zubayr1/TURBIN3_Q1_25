61cc8c559647a18378205f922b9e73d07118e8e2
zubayr1 <write2zubayr@gmail.com>
2025-02-23 20:11:50 +0100

    Update textify: add permissions

---
+++ capstone_project/one_cs/src/app/textify/page.tsx
@@ -0,0 +1,5 @@
+import OneCsFeature from "@/components/textify/one_cs-feature";
+
+export default function Page() {
+  return <OneCsFeature />;
+}
---
+++ capstone_project/one_cs/src/components/textify/one_cs-feature.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletButton } from "../solana/solana-provider";
+import { AppHero, ellipsify } from "../ui/ui-layout";
+import { ExplorerLink } from "../cluster/cluster-ui";
+import { useOneCsProgram } from "./one_cs-data-access";
+import { OneCsCreate, OneCsList } from "./one_cs-ui";
+
+export default function OneCsFeature() {
+  const { publicKey } = useWallet();
+  const { programId } = useOneCsProgram();
+
+  return publicKey ? (
+    <div>
+      <AppHero
+        title="Textify"
+        subtitle={
+          "Textify is a 1CS protocol that allows users to encapsulate text data and share it with others."
+        }
+      >
+        <p className="mb-6">
+          <ExplorerLink
+            path={`account/${programId}`}
+            label={ellipsify(programId.toString())}
+          />
+        </p>
+        <OneCsCreate />
+      </AppHero>
+      <OneCsList />
+    </div>
+  ) : (
+    <div className="max-w-4xl mx-auto">
+      <div className="hero py-[64px]">
+        <div className="hero-content text-center">
+          <WalletButton />
+        </div>
+      </div>
+    </div>
+  );
+}
