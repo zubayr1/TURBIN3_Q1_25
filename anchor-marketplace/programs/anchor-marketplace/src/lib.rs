3510a000bfa960d0cc15bfba65ee9ea54a284631
zubayr1 <write2zubayr@gmail.com>
2025-03-02 21:32:46 +0100

    implement initialize fn

--- anchor-marketplace/programs/anchor-marketplace/src/lib.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/lib.rs
@@ -1,16 +1,18 @@
 use anchor_lang::prelude::*;
 
-declare_id!("J4vjbcL2LJPqTx7vJBRJcgrnJQ4dB6optWXkzgMJinbB");
+mod instructions;
+mod states;
+
+use instructions::*;
+use states::*;
+
+declare_id!("Hd1G3Newy87gKKbDMx9sHSYH9FnGUewKxxFeEVmxpPxy");
 
 #[program]
 pub mod anchor_marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<InitializeMarketplace>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.initialize(name, fee, &ctx.bumps)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
