df4803f805c3a5b333e6ba5b89a2d3571e4946ca
zubayr1 <write2zubayr@gmail.com>
2025-03-06 21:28:37 +0100

    add functions

--- anchor-marketplace/programs/anchor-marketplace/src/lib.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/lib.rs
@@ -15,4 +15,16 @@ pub mod anchor_marketplace {
     pub fn initialize(ctx: Context<InitializeMarketplace>, name: String, fee: u16) -> Result<()> {
         ctx.accounts.initialize(name, fee, &ctx.bumps)
     }
+
+    pub fn create_listing(ctx: Context<CreateListing>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<CreateListing>) -> Result<()> {
+        ctx.accounts.deposit()
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.delist()
+    }
 }
