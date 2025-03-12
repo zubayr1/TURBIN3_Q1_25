ba599c5f02a5ed540fbdf084bf897da2b8e90341
zubayr1 <write2zubayr@gmail.com>
2025-03-12 23:45:16 +0100

    update edit token data functions

--- capstone_project/one_cs/anchor/programs/one_cs/src/instructions/edit_data.rs
+++ capstone_project/one_cs/anchor/programs/one_cs/src/instructions/edit_data.rs
@@ -58,9 +58,158 @@ impl<'info> EditTextData<'info> {
     }
 }
 
+fn check_time_limited<'info>(
+    payer_permissioned_wallet: &Account<'info, PermissionedWallet>,
+) -> Result<()> {
+    // Check if time limited and if so, check if the current time is between the start and end time
+    if payer_permissioned_wallet.role == Role::TimeLimited {
+        let current_time = Clock::get()?.unix_timestamp as u64;
+        if current_time < payer_permissioned_wallet.start_time
+            || current_time > payer_permissioned_wallet.end_time
+        {
+            return Err(error!(ErrorCode::Unauthorized));
+        }
+    }
+
+    Ok(())
+}
+
 #[derive(Accounts)]
 #[instruction(label: String)]
-pub struct EditTokenData<'info> {
+pub struct EditDepositTokenData<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub creator: SystemAccount<'info>,
+
+    pub token_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        has_one = token_mint,
+        seeds = [b"escrow", creator.key().as_ref(), label.as_ref()],
+        bump = escrow.bump
+      )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = escrow,
+      )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = payer,
+    )]
+    pub payer_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        seeds = [b"permissioned_wallet".as_ref(), payer.key().as_ref(), label.as_ref()],
+        bump = payer_permissioned_wallet.bump,
+        constraint = payer_permissioned_wallet.main_data_pda == encapsulated_data.key()
+            @ ErrorCode::Unauthorized
+    )]
+    pub payer_permissioned_wallet: Account<'info, PermissionedWallet>,
+
+    #[account(
+        mut,
+        seeds = [b"permissions", creator.key().as_ref(), label.as_ref()],
+        bump = encapsulated_data.bump,
+        realloc = 8 + PermissionData::INIT_SPACE,
+        realloc::payer = payer,
+        realloc::zero = true
+      )]
+    pub encapsulated_data: Account<'info, PermissionData>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> EditDepositTokenData<'info> {
+    pub fn edit_deposit_token_data(&mut self, label: String, amount: u64) -> Result<()> {
+        check_time_limited(&self.payer_permissioned_wallet)?;
+
+        let encapsulated_data = &mut self.encapsulated_data;
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.payer_ata.to_account_info(),
+            to: self.vault.to_account_info(),
+            mint: self.token_mint.to_account_info(),
+            authority: self.payer.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_ctx, amount, self.token_mint.decimals)?;
+
+        encapsulated_data.data = EncapsulatedData {
+            text: None,
+
+            token: Some(TokenData {
+                token_mint: self.token_mint.key(),
+                token_amount: encapsulated_data
+                    .data
+                    .token
+                    .as_ref()
+                    .unwrap()
+                    .token_amount
+                    .checked_add(amount)
+                    .ok_or(error!(ErrorCode::AmountTooLarge))?,
+            }),
+        };
+
+        Ok(())
+    }
+}
+
+fn close_escrow<'info>(
+    vault: &InterfaceAccount<'info, TokenAccount>,
+    owner: &SystemAccount<'info>,
+    escrow: &Account<'info, Escrow>,
+    creator: &SystemAccount<'info>,
+    token_program: &Interface<'info, TokenInterface>,
+    encapsulated_data: &mut Account<'info, PermissionData>,
+    label: String,
+) -> Result<()> {
+    let cpi_program = token_program.to_account_info();
+
+    let cpi_accounts = CloseAccount {
+        account: vault.to_account_info(),
+        destination: owner.to_account_info(),
+        authority: escrow.to_account_info(),
+    };
+
+    let binding = creator.key();
+
+    let seeds = &[b"escrow", binding.as_ref(), label.as_ref(), &[escrow.bump]];
+    let signer_seeds = &[&seeds[..]];
+
+    let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+    close_account(cpi_ctx)?;
+
+    empty_encapsulated_data(encapsulated_data)?;
+
+    Ok(())
+}
+
+fn empty_encapsulated_data(encapsulated_data: &mut Account<'_, PermissionData>) -> Result<()> {
+    encapsulated_data.data = EncapsulatedData {
+        text: None,
+        token: None,
+    };
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(label: String)]
+pub struct EditTransferTokenData<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
 
@@ -131,63 +280,10 @@ pub struct EditTokenData<'info> {
     pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
-impl<'info> EditTokenData<'info> {
-    pub fn edit_token_data(&mut self, label: String, amount: u64, is_deposit: bool) -> Result<()> {
-        // Check if time limited and if so, check if the current time is between the start and end time
-        if self.payer_permissioned_wallet.role == Role::TimeLimited {
-            let current_time = Clock::get()?.unix_timestamp as u64;
-            if current_time < self.payer_permissioned_wallet.start_time
-                || current_time > self.payer_permissioned_wallet.end_time
-            {
-                return Err(error!(ErrorCode::Unauthorized));
-            }
-        }
+impl<'info> EditTransferTokenData<'info> {
+    pub fn edit_transfer_token_data(&mut self, label: String, amount: u64) -> Result<()> {
+        check_time_limited(&self.payer_permissioned_wallet)?;
 
-        if is_deposit {
-            self.deposit_token_data(label, amount)?;
-        } else {
-            self.withdraw_token_data(label, amount)?;
-        }
-
-        Ok(())
-    }
-
-    fn deposit_token_data(&mut self, label: String, amount: u64) -> Result<()> {
-        let encapsulated_data = &mut self.encapsulated_data;
-
-        let cpi_program = self.token_program.to_account_info();
-
-        let cpi_accounts = TransferChecked {
-            from: self.payer_ata.to_account_info(),
-            to: self.vault.to_account_info(),
-            mint: self.token_mint.to_account_info(),
-            authority: self.payer.to_account_info(),
-        };
-
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-
-        transfer_checked(cpi_ctx, amount, self.token_mint.decimals)?;
-
-        encapsulated_data.data = EncapsulatedData {
-            text: None,
-
-            token: Some(TokenData {
-                token_mint: self.token_mint.key(),
-                token_amount: encapsulated_data
-                    .data
-                    .token
-                    .as_ref()
-                    .unwrap()
-                    .token_amount
-                    .checked_add(amount)
-                    .ok_or(error!(ErrorCode::AmountTooLarge))?,
-            }),
-        };
-
-        Ok(())
-    }
-
-    fn withdraw_token_data(&mut self, label: String, amount: u64) -> Result<()> {
         let encapsulated_data = &mut self.encapsulated_data;
 
         let cpi_program = self.token_program.to_account_info();
@@ -199,9 +295,10 @@ impl<'info> EditTokenData<'info> {
             authority: self.escrow.to_account_info(),
         };
 
+        let binding = self.creator.key();
         let seeds = &[
             b"escrow",
-            self.creator.key.as_ref(),
+            binding.as_ref(),
             label.as_ref(),
             &[self.escrow.bump],
         ];
@@ -211,11 +308,18 @@ impl<'info> EditTokenData<'info> {
         transfer_checked(cpi_ctx, amount, self.token_mint.decimals)?;
 
         if amount == encapsulated_data.data.token.as_ref().unwrap().token_amount {
-            self.close_escrow(label)?;
+            close_escrow(
+                &self.vault,
+                &self.owner,
+                &self.escrow,
+                &self.creator,
+                &self.token_program,
+                encapsulated_data,
+                label,
+            )?;
         } else {
             encapsulated_data.data = EncapsulatedData {
                 text: None,
-
                 token: Some(TokenData {
                     token_mint: self.token_mint.key(),
                     token_amount: encapsulated_data
@@ -232,38 +336,120 @@ impl<'info> EditTokenData<'info> {
 
         Ok(())
     }
+}
 
-    fn close_escrow(&mut self, label: String) -> Result<()> {
+#[derive(Accounts)]
+#[instruction(label: String)]
+pub struct EditWithdrawTokenData<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub creator: SystemAccount<'info>,
+
+    pub owner: SystemAccount<'info>,
+
+    pub token_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        has_one = token_mint,
+        seeds = [b"escrow", creator.key().as_ref(), label.as_ref()],
+        bump = escrow.bump
+      )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = escrow,
+      )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = token_mint,
+        associated_token::authority = owner,
+    )]
+    pub owner_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        seeds = [b"permissioned_wallet".as_ref(), payer.key().as_ref(), label.as_ref()],
+        bump = payer_permissioned_wallet.bump,
+        constraint = payer_permissioned_wallet.main_data_pda == encapsulated_data.key()
+            @ ErrorCode::Unauthorized
+    )]
+    pub payer_permissioned_wallet: Account<'info, PermissionedWallet>,
+
+    #[account(
+        mut,
+        seeds = [b"permissions", creator.key().as_ref(), label.as_ref()],
+        bump = encapsulated_data.bump,
+        realloc = 8 + PermissionData::INIT_SPACE,
+        realloc::payer = payer,
+        realloc::zero = true
+      )]
+    pub encapsulated_data: Account<'info, PermissionData>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> EditWithdrawTokenData<'info> {
+    pub fn edit_withdraw_token_data(&mut self, label: String, amount: u64) -> Result<()> {
+        check_time_limited(&self.payer_permissioned_wallet)?;
+
+        let encapsulated_data = &mut self.encapsulated_data;
+
         let cpi_program = self.token_program.to_account_info();
 
-        let cpi_accounts = CloseAccount {
-            account: self.vault.to_account_info(),
-            destination: self.owner.to_account_info(),
+        let cpi_accounts = TransferChecked {
+            from: self.vault.to_account_info(),
+            to: self.owner_ata.to_account_info(),
+            mint: self.token_mint.to_account_info(),
             authority: self.escrow.to_account_info(),
         };
 
+        let binding = self.creator.key();
         let seeds = &[
             b"escrow",
-            self.creator.key.as_ref(),
+            binding.as_ref(),
             label.as_ref(),
             &[self.escrow.bump],
         ];
         let signer_seeds = &[&seeds[..]];
 
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        transfer_checked(cpi_ctx, amount, self.token_mint.decimals)?;
 
-        close_account(cpi_ctx)?;
+        if amount == encapsulated_data.data.token.as_ref().unwrap().token_amount {
+            close_escrow(
+                &self.vault,
+                &self.owner,
+                &self.escrow,
+                &self.creator,
+                &self.token_program,
+                encapsulated_data,
+                label,
+            )?;
+        } else {
+            encapsulated_data.data = EncapsulatedData {
+                text: None,
+                token: Some(TokenData {
+                    token_mint: self.token_mint.key(),
+                    token_amount: encapsulated_data
+                        .data
+                        .token
+                        .as_ref()
+                        .unwrap()
+                        .token_amount
+                        .checked_sub(amount)
+                        .ok_or(error!(ErrorCode::AmountTooLarge))?,
+                }),
+            };
+        }
 
-        self.empty_encapsulated_data()?;
-
-        Ok(())
-    }
-
-    fn empty_encapsulated_data(&mut self) -> Result<()> {
-        self.encapsulated_data.data = EncapsulatedData {
-            text: None,
-            token: None,
-        };
         Ok(())
     }
 }
